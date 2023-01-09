2bf3ddd40cc995dc4bc233a55536816983f8cb6d
Jacob Hinkle <jhinkle@nvidia.com>
2023-01-09 10:28:02 -0500

    WIP converting forward to Python interface
    I still do not properly handle casting of the output when running stats are used. Also the test currently fails with "RuntimeError: Dtype is not supported:float".

--- pynvfinstnorm/instance_norm_nvfuser.py
+++ pynvfinstnorm/instance_norm_nvfuser.py
@@ -4,13 +4,15 @@ import torch
 from torch import Tensor
 from torch.nn.modules.batchnorm import _NormBase
 
-from nvfuser._C import Fusion, FusionCache, FusionDefinition, DataType
+from nvfuser._C import DataType, Fusion, FusionCache, FusionDefinition, Scalar, Tensor
+
+from typing import Any, Optional, Tuple
 
 
 __all__ = ["InstanceNormNVFuserFunction", "InstanceNorm3dNVFuser"]
 
 
-def torch2datatype(dt: torch.dtype) -> DataType:
+def torch2datatype(dt: torch.dtype) -> Optional[DataType]:
     """Translate between PyTorch and NVFuser element types.
 
     Returns `None` if the type cannot be translated.
@@ -26,24 +28,146 @@ def torch2datatype(dt: torch.dtype) -> DataType:
     }.get(dt)
 
 
+def instance_norm(
+    fd: FusionDefinition,
+    x: Tensor,
+    weight: Optional[Tensor],
+    bias: Optional[Tensor],
+    running_mean: Optional[Tensor],
+    running_var: Optional[Tensor],
+    use_input_stats: bool,
+    momentum: Scalar,
+    eps: Scalar,
+    channels_last: bool,
+    extent: torch.Size,
+) -> Tensor:
+    """Compute instance norm layer forward for arbitrary dimensional input.
+
+    This is a translation of `instance_norm` in NVFuser [^1] which is not
+    exposed currently in the Python frontend
+
+    [^1]: https://github.com/csarofeen/pytorch/blob/devel/third_party/nvfuser/csrc/ops/normalization.cpp#L710
+    """
+    assert not (
+        (running_var is None) ^ (running_mean is None)
+    ), "Iff running mean or var is given, the other should be"
+
+    kBatchDim = 0
+    kNumberOfDims = len(extent)
+    kChannelsDim = kNumberOfDims - 1 if channels_last else 1
+
+    x_reduction_axes = [
+        axis for axis in range(kNumberOfDims) if axis not in [kBatchDim, kChannelsDim]
+    ]
+    x_broadcast_mask = [axis in x_reduction_axes for axis in range(kNumberOfDims)]
+    B = fd.define_constant(extent[kBatchDim])
+
+    channels_only_broadcast_mask = [
+        axis != kChannelsDim for axis in range(kNumberOfDims)
+    ]
+
+    y = None
+    mean = None
+    invstd = None
+    if use_input_stats or running_mean is None:
+        # In NVFuser Python we pass correction=1 to request unbiased variance calculation
+        x_var, x_mean = fd.ops.var_mean(x, x_reduction_axes, 1)
+        if running_mean is not None and running_var is not None:
+            rev_momentum = fd.ops.sub(fd.define_constant(1.0), momentum)
+            # do running mean with momentum
+            current_mean_hat = fd.ops.mul(x_mean, momentum)
+            mean_hat = fd.ops.mul(running_mean, rev_momentum)
+            new_mean_hat = fd.ops.add(mean_hat, current_mean_hat)
+            """
+              if (running_mean->getDataType().value() == DataType::Half ||
+                  running_mean->getDataType().value() == DataType::BFloat16) {
+                new_mean_channels_only =
+                    castOp(running_mean->getDataType().value(), new_mean_channels_only);
+              }
+            """
+            new_mean_sum = fd.ops.sum(new_mean_hat, [kBatchDim])
+            rB = fd.ops.reciprocal(B)
+            new_mean_channels_only = fd.ops.mul(new_mean_sum, rB)
+            # TODO: cast new_mean_channels_only to type of running_mean
+
+            # TODO: is this a sufficient alternative to aliasOutputToInput?
+            running_mean = new_mean_channels_only
+
+            # running var calculation
+            current_var_hat = fd.ops.mul(x_var, momentum)
+            var_hat = fd.ops.mul(running_var, rev_momentum)
+            new_var_hat = fd.ops.add(var_hat, current_var_hat)
+
+            new_var_sum = fd.ops.sum(new_var_hat, [kBatchDim])
+            new_var_channels_only = fd.ops.mul(new_mean_sum, rB)
+            # TODO: Alias to input running_var
+            # fd.aliasOutputToInput(new_var_channels_only, running_var)
+            running_var = new_var_channels_only
+
+        mean = x_mean
+        mean_bcast = fd.ops.broadcast(mean, x_broadcast_mask)
+        x_sub_mean = fd.ops.sub(x, mean_bcast)
+
+        var_eps = fd.ops.add(x_var, eps)
+        invstd = fd.ops.rsqrt(var_eps)
+        invstd_bcast = fd.ops.broadcast(invstd, x_broadcast_mask)
+
+        y = fd.ops.mul(x_sub_mean, invstd_bcast)
+
+    else:  # This is inference mode with running stats
+        """
+        } else {
+          // This is inference mode with running stats
+          auto r_mean_bcasted = broadcast(running_mean, channels_only_broadcast_mask);
+          auto x_sub_mean = sub(x, r_mean_bcasted);
+
+          auto var_eps = add(running_var, eps);
+          auto unbiased_invstd = rsqrt(var_eps);
+          auto invstd_bcast =
+              broadcast(unbiased_invstd, channels_only_broadcast_mask);
+        """
+        r_mean_bcasted = fd.ops.broadcast(running_mean, channels_only_broadcast_mask)
+        x_sub_mean = fd.ops.sub(x, r_mean_bcasted)
+
+        var_eps = fd.ops.add(running_var, eps)
+        unbiased_invstd = fd.ops.rsqrt(var_eps)
+        invstd_bcast = fd.ops.broadcast(unbiased_invstd, channels_only_broadcast_mask)
+
+        # During inference, mean/invstd output are empty tensors
+        # on CPU, but not on CUDA. We need to make sure we have the same
+        # behavior as with eager mode on CUDA.
+        mean = running_mean
+        invstd = unbiased_invstd
+        y = fd.ops.mul(x_sub_mean, invstd_bcast)
+
+    if weight is not None:
+        weight_bcast = fd.ops.broadcast(weight, channels_only_broadcast_mask)
+        y = fd.ops.mul(y, weight_bcast)
+    if bias is not None:
+        bias_bcast = fd.ops.broadcast(bias, channels_only_broadcast_mask)
+        y = fd.ops.mul(y, bias_bcast)
+
+    return y, mean, invstd
+
+
 class InstanceNormNVFuserFunction(torch.autograd.Function):
     @staticmethod
     def forward(
-        ctx,
-        x,
-        weight,
-        bias,
-        running_mean,
-        running_var,
-        use_input_stats,
-        momentum,
-        eps,
-    ):
+        ctx: Any,  # contexts are actually objects of the type we are currently defining
+        x: torch.Tensor,
+        weight: Optional[torch.Tensor],
+        bias: Optional[torch.Tensor],
+        running_mean: Optional[torch.Tensor],
+        running_var: Optional[torch.Tensor],
+        use_input_stats: bool,
+        momentum: float,
+        eps: float,
+    ) -> torch.Tensor:
         channels_last = x.is_contiguous(
             memory_format=torch.channels_last
         ) or x.is_contiguous(memory_format=torch.channels_last_3d)
         if channels_last:
-            order = [0] + [i for i in range(2, len(input.shape))] + [1]
+            order = [0] + [i for i in range(2, len(x.shape))] + [1]
             _input = x.permute(order)
         else:
             _input = x
@@ -54,49 +178,65 @@ class InstanceNormNVFuserFunction(torch.autograd.Function):
         fd = FusionDefinition(fs)
         with fd:
             tv_x = fd.define_tensor(x.ndim, torch2datatype(x.dtype))
-            tv_weight = fd.define_tensor(weight.ndim, torch2datatype(weight.dtype))
-            tv_bias = fd.define_tensor(bias.ndim, torch2datatype(bias.dtype))
+            if weight is not None:
+                assert bias is not None
+                tv_weight = fd.define_tensor(weight.ndim, torch2datatype(weight.dtype))
+                tv_bias = fd.define_tensor(bias.ndim, torch2datatype(bias.dtype))
+            else:
+                tv_weight = None
+                tv_bias = None
             inputs = [x, weight, bias]
 
             if running_mean is None:
                 tv_running_mean = None
                 tv_running_var = None
             else:
+                assert running_var is not None
                 tv_running_mean = fd.define_tensor(
-                    running_mean.shape, torch2datatype(running_mean.dtype)
+                    running_mean.ndim, torch2datatype(running_mean.dtype)
                 )
                 tv_running_var = fd.define_tensor(
-                    running_var.shape, torch2datatype(running_var.dtype)
+                    running_var.ndim, torch2datatype(running_var.dtype)
                 )
-                inputs = inputs.extend([running_mean, running_var])
+                inputs.extend([running_mean, running_var])
+                if running_mean.dtype in [torch.half, torch.bfloat16]:
+                    tv_running_mean = fd.ops.castOp(DataType.Float, tv_running_mean)
+                if running_var.dtype in [torch.half, torch.bfloat16]:
+                    tv_running_var = fd.ops.castOp(DataType.Float, tv_running_var)
 
-            # NOTE: original C++ code defined these as inputs, but unless these
-            # change during runtime it may be better to treat them as constants, right?
-            c_momentum = fd.define_constant(momentum)
-            c_eps = fd.define_constant(eps)
+            s_momentum = fd.define_scalar(DataType.Float)
+            s_eps = fd.define_scalar(DataType.Float)
+            inputs.extend([momentum, eps])
 
             # cast inputs if necessary
-            if x.dtype != torch.float32:
+            if x is not None and x.dtype in [torch.half, torch.bfloat16]:
                 tv_x = fd.ops.castOp(DataType.Float, tv_x)
-            if weight.dtype != torch.float32:
+            if weight is not None and weight.dtype in [torch.half, torch.bfloat16]:
                 tv_weight = fd.ops.castOp(DataType.Float, tv_weight)
-            if bias.dtype != torch.float32:
+            if bias is not None and bias.dtype in [torch.half, torch.bfloat16]:
                 tv_bias = fd.ops.castOp(DataType.Float, tv_bias)
 
-            result = fd.ops.instance_norm(
+            # TODO: implement instance_norm here using primitives from fd.ops,
+            # instead of the built-in instance_norm which is not exposed in the
+            # Python frontend
+            out, mean, invstd = instance_norm(
+                fd,
                 tv_x,
                 tv_weight,
                 tv_bias,
                 tv_running_mean,
                 tv_running_var,
                 use_input_stats,
-                momentum,
-                eps,
+                s_momentum,
+                s_eps,
                 channels_last,
+                extent=x.shape,
             )
 
-            fd.add_output(result)
-        result = fs.execute(inputs)[0]
+            fd.add_output(out)
+            fd.add_output(mean)
+            fd.add_output(invstd)
+        out, mean, invstd = fs.execute(inputs)[0]
 
         ctx.use_input_stats = use_input_stats
         ctx.eps = eps
@@ -119,7 +259,9 @@ class InstanceNormNVFuserFunction(torch.autograd.Function):
         return out
 
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(
+        ctx: Any, grad_output: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, None, None, None, None, None,]:
         global instance_norm_nvfuser_cuda
         if instance_norm_nvfuser_cuda is None:
             instance_norm_nvfuser_cuda = importlib.import_module(
